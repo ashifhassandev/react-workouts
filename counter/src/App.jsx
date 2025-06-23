d8547115a8f89df5e4af6e6130210ab28dbe9f49
Ashif Hassan <ashifhassan1996@gmail.com>
2025-06-23 11:59:17 +0530

    Added reset functionality

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,19 +3,25 @@ import { useState } from 'react';
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const incrementCounter = () => {
-    setCount(count + 1);
-  };
+  const incrementCounter = () => setCount((count) => count + 1);
 
   const decrementCounter = () => {
-    setCount(count - 1);
+    if (count > 0) {
+      setCount((count) => count - 1);
+    }
   };
+  
+  const resetCounter = () => setCount(0);
 
   return (
     <div className='counter'>
-      <h1>{count}</h1>
-      <button onClick={incrementCounter} className='btn btn-primary'>+</button>
-      <button onClick={decrementCounter} className='btn btn-primary'>-</button>
+      <h1 style={{ marginBottom: '40px' }}>Counter App</h1>
+      <h2>{count}</h2>
+      <div className='buttons'>
+        <button onClick={incrementCounter} className='btn btn-primary'>+</button>
+        <button onClick={resetCounter} className='btn btn-primary'>Reset</button>
+        <button onClick={decrementCounter} className='btn btn-primary'>-</button>
+      </div>
     </div>
   );
 };
