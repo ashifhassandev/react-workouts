d9b044361f8377fa3d612e9517bb2b0940a7e69d
Ashif Hassan <ashifhassan1996@gmail.com>
2025-06-23 12:07:46 +0530

    Added reset functionality

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -10,17 +10,19 @@ const App = () => {
       setCount((count) => count - 1);
     }
   };
-  
+
   const resetCounter = () => setCount(0);
 
   return (
     <div className='counter'>
       <h1 style={{ marginBottom: '40px' }}>Counter App</h1>
-      <h2>{count}</h2>
-      <div className='buttons'>
-        <button onClick={incrementCounter} className='btn btn-primary'>+</button>
-        <button onClick={resetCounter} className='btn btn-primary'>Reset</button>
-        <button onClick={decrementCounter} className='btn btn-primary'>-</button>
+      <div className='box'>
+        <h2>{count}</h2>
+        <div className='buttons'>
+          <button onClick={incrementCounter} className='btn btn-primary'>+</button>
+          <button onClick={resetCounter} className='btn btn-primary'>Reset</button>
+          <button onClick={decrementCounter} className='btn btn-primary'>-</button>
+        </div>
       </div>
     </div>
   );
