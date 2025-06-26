9323a9a2aa8735c7e179fd556ad10b28d8a1813d
Ashif Hassan <ashifhassan1996@gmail.com>
2025-06-26 15:49:51 +0530

    Add react list workout

---
+++ lists/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+
+  const addItem = () => {
+    if (value.trim() === "") {
+      return;
+    }
+
+    setList((previousList) => [...previousList, value]);
+    setValue("");
+  };
+
+  return (
+    <div className="list-box">
+      <h1>React Lists</h1>
+      <div className="search-box">
+        <input 
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)} 
+          placeholder="Enter your text..."
+        />
+        <button onClick={addItem}>Add</button>
+      </div>
+      {list.length > 0 && (
+        <div className="list">
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
