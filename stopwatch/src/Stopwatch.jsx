535816bd292bb30790ae9dc686d84e6c3ad75071
Ashif Hassan <ashifhassan1996@gmail.com>
2025-08-03 11:52:55 +0530

    Add padZero function

--- stopwatch/src/Stopwatch.jsx
+++ stopwatch/src/Stopwatch.jsx
@@ -32,18 +32,15 @@ const Stopwatch = () => {
     setElapsedTime(0);
   };
 
+  const padZero = (number) => String(number).padStart(2, "0");
+
   const formatTime = () => {
-    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-    let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
-    let seconds = Math.floor((elapsedTime / 1000) % 60);
-    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    const minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    const seconds = Math.floor((elapsedTime / 1000) % 60);
+    const milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
-    hours = String(hours).padStart(2, "0");
-    minutes = String(minutes).padStart(2, "0");
-    seconds = String(seconds).padStart(2, "0");
-    milliseconds = String(milliseconds).padStart(2, "0");
-
-    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}:${padZero(milliseconds)}`;
   };
 
   return (
